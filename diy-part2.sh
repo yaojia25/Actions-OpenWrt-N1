1114aa113702b0340086bbdf654dded80e37f957
Jasyo <31342892+yaojia25@users.noreply.github.com>
2021-04-24 23:31:33 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,4 +11,7 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/10.0.0.2/g' package/base-files/files/bin/config_generate
+
+# firewall custom
+echo "iptables -t nat -I POSTROUTING -o eth0 -j MASQUERADE" >> package/network/config/firewall/files/firewall.user
